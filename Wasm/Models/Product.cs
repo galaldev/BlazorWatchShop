2d3c963252348619413251506e10373efc64d525
s <a>
2020-07-19 10:27:39 +0200

    Init

---
+++ Wasm/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Specifications { get; set; }
+        public List<int> RelatedIds { get; set; }
+    }
+    public class ProductItemVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
